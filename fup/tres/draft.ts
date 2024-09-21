fc802a4aa03ec84c65e2f6791ca401113b408a54
David Sena Oliveira <sena.ufc@gmail.com>
2024-09-21 20:45:11 +0000

    first

---
+++ fup/tres/draft.ts
@@ -0,0 +1,9 @@
+function input(): string { let X: any = input; X.L = X.L || require("fs").readFileSync(0).toString().split(/\r?\n/); return X.L.shift(); }
+// function input(): string { let X: any = input; X.P = X.P || require("readline-sync"); return X.P.question() }
+function write(text: any, endl="\n") { process.stdout.write("" + text + endl); }
+export {};
+
+function main() {
+    write("qxcode");
+}
+main();
